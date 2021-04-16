9e9bdd60a7358ca0006946e56c2a257e94dbc06e
bryan <bryanprz00@gmail.com>
2021-04-16 03:22:35 -0400

    Save whatever this is

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logoWhite from './logo-white.svg';
+import './App.scss';
+import {Image, Video, CloudinaryContext} from 'cloudinary-react'
+import {Cloudinary} from 'cloudinary-core'
 
 function App() {
+  const headerVideoURL = 'https://res.cloudinary.com/hkkczayke/video/upload/v1618553410/homepage/paramo-video_o15k7r.mp4'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is Paramo Software.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CloudinaryContext cloudName="hkkczayke">
+      <div className="App">
+        <header className="header-container">
+
+        <div>
+          <video autoplay muted loop id="header-video">
+            <source src={headerVideoURL} type="video/mp4" />
+          </video>
+        </div>
+
+        <img src={logoWhite} alt="paramosoftware-logo" />
+
+          <h1 className="header-primary">
+            We <br />
+            <span className="header-text-blue">design</span> and
+            <br />
+            <span className="header-text-red">develop </span><br />
+            web and mobile <br />applications
+          </h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </CloudinaryContext>
   );
 }
 
