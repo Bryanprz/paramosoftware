60945e565a7d94df980feab57e7070511fa5b06b
bryan <bryanprz00@gmail.com>
2021-04-17 01:44:17 -0400

    add company description start

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import Header from './Header'
 import {CloudinaryContext} from 'cloudinary-react'
+import Header from './Header'
+import CompanyDescription from './CompanyDescription'
 
 function App() {
   return (
     <CloudinaryContext cloudName="hkkczayke">
       <Header />
+      <CompanyDescription />
     </CloudinaryContext>
   );
 }
---
+++ src/components/CompanyDescription.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const CompanyDescription = props => (
+  <div className="company-description-container">
+    <h2 className="company-description-header">
+      Your Idea &#8594; Our Agency &#8594; Your New Product
+    </h2>
+
+    <div className="company-description-content-container">
+      <div className="company-description-first">
+        <h2 className="">
+          TURN YOUR IDEA INTO A REAL PRODUCT
+        </h2>
+
+      </div>
+
+      <div className="company-description-second">
+
+      </div>
+
+      <div className="company-description-third">
+
+      </div>
+    </div>
+  </div>
+)
+
+export default CompanyDescription
