03b787b8650546052ef5aac21bd3066e49187d51
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-17 19:14:27 -0500

    cartas con imagenes para la seccion de equipo

--- src/Cards.js
+++ src/Cards.js
@@ -1,53 +1,14 @@
-import logoWhite from './images/logo-white.svg';
-import './App.scss';
-import {Image, Video, CloudinaryContext} from 'cloudinary-react';
-import {Cloudinary} from 'cloudinary-core';
-import Bryan from './images/4745204.jpg';
-import Maria from './images/maria.png';
-import Jonatan from './images/jon.jpg';
-import Cristian from './images/profile.png';
+import React, { Component } from 'react';
 
 
-function App() {
-  const headerVideoURL = 'https://res.cloudinary.com/hkkczayke/video/upload/v1618553410/homepage/paramo-video_o15k7r.mp4'
-
-  return (
-    <div>
-        <CloudinaryContext cloudName="hkkczayke">
-          <div className="App">
-            <header className="header-container">
-
-            <div>
-              <video autoplay muted loop id="header-video">
-                <source src={headerVideoURL} type="video/mp4" />
-              </video>
-            </div>
-
-            <img src={logoWhite} alt="paramosoftware-logo" className="logo-top" />
-
-              <h1 className="header-primary">
-                We <br />
-                <span className="header-text-blue">design</span> and
-                <br />
-                <span className="header-text-red">develop </span><br />
-                web and mobile <br />applications
-              </h1>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
-        </CloudinaryContext>
-        <section className="cards">
+export default class Cards extends Component {
+    render() {
+        return (
+            <section className="cards">
                     <div className="container">
                         <div className="card">
                             <div className="content">
-                                <div className="imgBx"><img src={Bryan}alt='profile'></img></div>
+                                <div className="imgBx"><img src="./images/4745204.jpg "alt='profile'></img></div>
                                 <div className="contentBx">
                                     <h3 className="title">Bryan<br/><span class="span">Lorem, ipsum dolor.</span></h3>
                                 </div>
@@ -66,7 +27,7 @@ function App() {
                         </div>
                         <div className="card">
                             <div className="content">
-                                <div className="imgBx"><img src={Maria} alt='profile'></img></div>
+                                <div className="imgBx"><img src="./images/maria.png" alt='profile'></img></div>
                                 <div className="contentBx">
                                     <h3 className="title">Maria Isabel<br/><span class="span">Lorem, ipsum dolor.</span></h3>
                                 </div>
@@ -85,7 +46,7 @@ function App() {
                         </div>
                         <div className="card">
                             <div className="content">
-                                <div className="imgBx"><img src={Jonatan} alt='profile'></img></div>
+                                <div className="imgBx"><img src="./images/jon.jpg" alt='profile'></img></div>
                                 <div className="contentBx">
                                     <h3 className="title">Jonatan Osorio<br/><span class="span">Lorem, ipsum dolor.</span></h3>
                                 </div>
@@ -104,7 +65,7 @@ function App() {
                         </div>
                         <div className="card">
                             <div className="content">
-                                <div className="imgBx"><img src={Cristian} alt='profile'></img></div>
+                                <div className="imgBx"><img src="./images/profile.png" alt='profile'></img></div>
                                 <div className="contentBx">
                                     <h3 className="title">Cristian Loaiza<br/><span class="span">Lorem, ipsum dolor.</span></h3>
                                 </div>
@@ -123,8 +84,6 @@ function App() {
                         </div>
                     </div>
             </section>
-    </div>
-  );
+        )
+    }
 }
-
-export default App;
