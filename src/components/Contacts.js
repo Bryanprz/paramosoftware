17a506c1d66bb325551ba1409baacc715c60b2b3
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-30 20:10:43 -0500

    + enlaces a los iconos de la seccion contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -8,53 +8,46 @@ export default class Contacts extends Component {
         return (
             <div>
                 <h2 className="title-contacs">contact us!</h2>
-                <section class="icons">
+                <section className="icons">
                         
-                        <ul class="icons-ul">
-                            <li class="icons-ul__li">
-                                <a href="https://github.com/Bryanprz/paramosoftware/tree/team/src/components">
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span  class="fa fa-facebook icons-ul__li--span" aria-hidden="true"></span>
+                        <ul className="icons-ul">
+                            <li className="icons-ul__li">
+                                <a href="https://www.facebook.com/paramo.softwar" target="__new">
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span  className="fa fa-facebook icons-ul__li--span" aria-hidden="true"></span>
                                 </a>
                             </li>
 
-                            <li class="icons-ul__li">
-                                <a href="https://github.com/Bryanprz/paramosoftware/tree/team/src/components">
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="fa fa-twitter icons-ul__li--span" aria-hidden="true"></span>
+                            <li className="icons-ul__li">
+                                <a href="https://twitter.com/ParamoSoftware" target="__new">
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="fa fa-twitter icons-ul__li--span" aria-hidden="true"></span>
                                 </a>
                             </li>
-                            <li class="icons-ul__li">
-                                <a href="https://github.com/Bryanprz/paramosoftware/tree/team/src/components">
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span  class="fa fa-google-plus icons-ul__li--span" aria-hidden="true"></span>
+
+                            <li className="icons-ul__li">
+                                <a href="https://www.instagram.com/paramosoftware" target="__new">
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span  className="fa fa-instagram icons-ul__li--span" aria-hidden="true"></span>
                                 </a>
                             </li>
-                            <li class="icons-ul__li">
-                                <a href="https://github.com/Bryanprz/paramosoftware/tree/team/src/components">
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span  class="fa fa-linkedin icons-ul__li--span" aria-hidden="true"></span>
-                                </a>
-                            </li>
-                            <li class="icons-ul__li">
-                                <a href="https://github.com/Bryanprz/paramosoftware/tree/team/src/components">
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span class="icons-ul__li--span"></span>
-                                    <span  class="fa fa-instagram icons-ul__li--span" aria-hidden="true"></span>
+
+                            <li className="icons-ul__li">
+                                <a href="mailto:paramosoftware@gmail.com" target="__new">
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="icons-ul__li--span"></span>
+                                    <span className="fa fa-envelope icons-ul__li--span" aria-hidden="true"></span>
                                 </a>
                             </li>
                         </ul>
